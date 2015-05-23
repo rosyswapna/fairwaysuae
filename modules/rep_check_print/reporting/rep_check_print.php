499cbae8444ac36095ae2a7473f9de898c2b06b8
rosyswapna <rosy.swapna@gmail.com>
2015-05-23 15:27:28 +0530

    cheque printing

--- modules/rep_check_print/reporting/rep_check_print.php
+++ modules/rep_check_print/reporting/rep_check_print.php
@@ -29,21 +29,22 @@ include_once($path_to_root . "/includes/data_checks.inc");
 print_check();
 
 //----------------------------------------------------------------------------------------------------
-function get_remittance($type, $trans_no)
+function get_remittance($counter)
 {
-   	$sql = "SELECT ".TB_PREF."supp_trans.*, 
-   		(".TB_PREF."supp_trans.ov_amount+".TB_PREF."supp_trans.ov_gst+".TB_PREF."supp_trans.ov_discount) AS Total, 
-   		".TB_PREF."suppliers.supp_name,  ".TB_PREF."suppliers.supp_account_no,
-   		".TB_PREF."suppliers.curr_code, ".TB_PREF."suppliers.payment_terms, ".TB_PREF."suppliers.gst_no AS tax_id, 
-   		".TB_PREF."suppliers.address, ".TB_PREF."suppliers.contact
-		FROM ".TB_PREF."supp_trans, ".TB_PREF."suppliers
-		WHERE ".TB_PREF."supp_trans.supplier_id = ".TB_PREF."suppliers.supplier_id
-		AND ".TB_PREF."supp_trans.type = ".db_escape($type)."
-		AND ".TB_PREF."supp_trans.trans_no = ".db_escape($trans_no);
+   	$sql = "SELECT gl_trans.tran_date,gl_trans.amount AS Total, 
+		if(person_type_id =".PT_CUSTOMER.",debtor.name,
+		if(person_type_id =".PT_SUPPLIER.",supplier.supp_name,'')) AS Name
+   		
+		FROM ".TB_PREF."gl_trans gl_trans
+		LEFT JOIN ".TB_PREF."debtors_master debtor ON debtor.debtor_no = person_id
+		LEFT JOIN ".TB_PREF."suppliers supplier ON supplier.supplier_id = person_id
+		WHERE counter = ".db_escape($counter);
+			
+		
    	$result = db_query($sql, "The remittance cannot be retrieved");
    	if (db_num_rows($result) == 0)
    		return false;
-    return db_fetch($result);
+    	return db_fetch($result);
 }
 
 function get_allocations_for_remittance($supplier_id, $type, $trans_no)
@@ -67,148 +68,65 @@ function print_check()
     global $path_to_root, $systypes_array, $print_invoice_no;
 
     // Get the payment
-		$fromd = $_POST['PARAM_0'];
-	$tod = $_POST['PARAM_1'];
-	$fromd = date2sql($fromd);
-	$tod = date2sql($tod);
-
-    $from = $_POST['PARAM_2'];
-    $destination = $_POST['PARAM_3'];
+	$counter = $_POST['PARAM_0'];
+	$issueDate = $_POST['PARAM_1'];
+	$payeName = $_POST['PARAM_2'];
+	$amount = $_POST['PARAM_3'];
+    	$destination = $_POST['PARAM_4'];
  	
- 	$trans_no = explode("-", $from);
-
+ 	
 	$dec = user_price_dec();
 	if ($destination)
 		include_once($path_to_root . "/reporting/includes/excel_report.inc");
 	else
 		include_once($path_to_root . "/reporting/includes/pdf_report.inc");
-    
-	$from_trans = get_remittance($trans_no[1], $trans_no[0]);
 
-    // Get check information
-    $total_amt = $from_trans['Total'];
+	if($counter > 0){
+		$from_trans = get_remittance($counter);
+		$Name = $from_trans['Name'];
+		$total_amt = abs($from_trans['Total']);
+   		$date = sql2date($from_trans['tran_date']);
+	}else{
+		$Name = $payeName;
+		$total_amt = (float)$amount;
+   		$date = $issueDate;
+	}
     
-    $date = sql2date($from_trans['tran_date']);
-    $memo = get_comments_string($trans_no[1], $trans_no[0]);
-    
-    // Begin the report
-    $rep = new FrontReport(_('Printable Check'), "PrintableCheck", user_pagesize());
+
+	// Get check information
+   	
+    	// Begin the report
+    	$rep = new FrontReport(_('Printable Check'), "PrintableCheck", user_pagesize());
 	$rep->SetHeaderType(null);    
-    $rep->NewPage();
-    // Set the font
-    $rep->Font('','courier');
-    $rep->fontSize = 12;
+    	$rep->NewPage();
+    	// Set the font
+   	 $rep->Font('','courier');
+    	$rep->fontSize = 12;
    
-    //////////////////
-    // Check portion
+    	//////////////////
+    	// Check portion
     
-    $rep->NewLine(1,0,76);
-    $rep->cols = array(63,300, 340);
-    $rep->aligns = array('left', 'left', 'left','right');
+    	$rep->NewLine(1,0,76);
+    	$rep->cols = array(63,300, 340);
+    	$rep->aligns = array('left', 'left', 'left','right');
     
-		// Date
-		$rep->NewLine(0,0,76);
-    $rep->DateCol(2,10, $rep->DatePrettyPrint($date, 0, 3));
+	// Date
+	$rep->NewLine(0,0,76);
+    	$rep->DateCol(2,10, $rep->DatePrettyPrint($date, 0, 3));
 
-    // Pay to  
-		$rep->NewLine(1,0,23);  
-    $rep->TextCol(0, 1, $from_trans['supp_name']);
+    	// Pay to  
+	$rep->NewLine(1,0,23);  
+    	$rep->TextCol(0, 1, $Name);
     
-    // Amount (words)
-		
-    $rep->NewLine(1,0,23);
-    $rep->TextCol(0, 2,  price_in_words_custom(-$total_amt));
+    	// Amount (words)	
+    	$rep->NewLine(1,0,23);
+    	$rep->TextCol(0, 2,  price_in_words_custom($total_amt));
     
-    // Amount (numeric)
-		$rep->NewLine(0.5,0,26);
-    $rep->TextCol(2, 10, number_format2(-$total_amt, $dec));
+    	// Amount (numeric)
+	$rep->NewLine(0.5,0,26);
+    	$rep->TextCol(2, 10, number_format2($total_amt, $dec));
     
-    
-    
-    // Memo
-   /* $rep->NewLine(1,0,78);
-    $rep->TextCol(0, 1, $memo);
-
-  	$rep->company = get_company_prefs();*/
-  /////////////////////
-    // Item details x 2 
-    
-    for ($section=1; $section<=0; $section++) 
-    {
-        $rep->fontSize = 12;        
-        // Move down to the correct section
-        $rep->row = $section == 1 ? 505 : 255;
-        $rep->cols = array(20, 340, 470, 588);
-        $rep->aligns = array('left', 'left', 'right', 'right');
-        
-        // Pay to
-        $rep->Font('b');
-        $rep->TextCol(0, 1, $from_trans['supp_name']);
-        $rep->Font();
-        
-        // Date
-        $rep->DateCol(1, 2, $rep->DatePrettyPrint($date, 0, 0));
-        
-        // Amount (numeric)
-        $rep->TextCol(2, 3, number_format2(-$total_amt, 2));
-    
-        // Add Trans # + Reference
-        $rep->NewLine();
-        if ($print_invoice_no == 0)
-        	$tno = $from_trans['reference'];
-        else
-        	$tno = $from_trans['trans_no'];
-        $rep->TextCol(0, 3, sprintf( _("Payment # %s - from Customer: %s - %s"), $tno,
-        	$from_trans['supp_account_no'], $rep->company['coy_name']));
-        
-        // Add memo
-        $rep->NewLine();
-        $rep->TextCol(0, 3, _("Memo: ").$memo);    
-        
-        // TODO: Do we want to set a limit on # of item details?  (Max is probably 6-7)
-
-        // Get item details
-		$result = get_allocations_for_remittance($from_trans['supplier_id'], $from_trans['type'], $from_trans['trans_no']);
-        // Fill in details
-        $rep->NewLine(2);
-        $rep->fontSize = 10;
-        // Use different columns now for the additional info
-        $rep->cols = array(20, 160, 235, 290, 370, 480, 588);
-        $rep->aligns = array('left', 'left', 'left', 'right', 'right', 'right');
-        
-        // Add headers
-        $rep->Font('b');
-        $rep->TextCol(0, 1, _("Type/Id"));
-        $rep->TextCol(1, 2, _("Trans Date"));
-        $rep->TextCol(2, 3, _("Due Date"));
-        $rep->TextCol(3, 4, _("Total Amount"));
-        $rep->TextCol(4, 5, _("Left to Allocate"));
-        $rep->TextCol(5, 6, _("This Allocation"));
-        $rep->NewLine();
-        
-        $rep->Font();    
-		$total_allocated = 0;
-		while ($item=db_fetch($result))
-		{
-           	$rep->TextCol(0, 1, $systypes_array[$item['type']]." ".$item['supp_reference']);
-           	$rep->TextCol(1, 2, sql2date($item['tran_date']));
-           	$rep->TextCol(2, 3, sql2date($item['due_date']));
-           	$rep->AmountCol(3, 4, $item['Total'], $dec);
-           	$rep->AmountCol(4, 5, $item['Total'] - $item['alloc'], $dec);
-           	$rep->AmountCol(5, 6, $item['amt'], $dec);
-           	$total_allocated += $item['amt'];
-       		$rep->NewLine(1, 0, $rep->lineHeight + 3); // Space it out
-    	}
-    	$rep->NewLine();
-       	$rep->TextCol(4, 5, _("Total Allocated"));
-       	$rep->AmountCol(5, 6, $total_allocated, $dec);
-    	$rep->NewLine();
-       	$rep->TextCol(4, 5, _("Left to Allocate"));
-       	$rep->AmountCol(5, 6, -$from_trans['Total'] - $total_allocated, $dec);
-    	
-    } // end of section
-    
-    $rep->End();
+   	$rep->End();
 }
 
 //--------------------------------------------------------------------------------
--- modules/rep_check_print/reporting/reports_custom.php
+++ modules/rep_check_print/reporting/reports_custom.php
@@ -4,8 +4,10 @@ global $reports, $dim;
 
 $reports->addReportClass(_('Check'), RC_CHECK);
 $reports->addReport(RC_CHECK  	, "_check_print",_('Printable &Check'),
-    array(  _('Start Date') => 'DATEBEGINM',
-			_('End Date') => 'DATEENDM',
-			_('Payment') => 'REMITTANCE',
-            _('Destination') => 'DESTINATION'));
+    array(
+	_('Transaction') => 'GL_REMITTANCE',
+	_("Issue Date") => 'DATE',
+	_("Payee Name") => 'TEXT',
+	_("Amount") => 'TEXT',
+        _('Destination') => 'DESTINATION'));
 ?>
