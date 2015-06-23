28b50d2a168e080b501af45caedb7dee760cdbe4
rosyswapna <rosy.swapna@gmail.com>
2015-06-23 16:38:57 +0530

    report payment advice

--- modules/payroll/reporting/repadvice.php
+++ modules/payroll/reporting/repadvice.php
@@ -9,6 +9,9 @@ $path_to_root="..";
 include_once($path_to_root . "/includes/session.inc");
 include_once($path_to_root . "/includes/date_functions.inc");
 include_once($path_to_root . "/includes/data_checks.inc");
+
+include_once($path_to_root . "/modules/payroll/includes/payroll_db.inc");
+
 add_access_extensions();
 
 print_advice_report();
@@ -25,26 +28,42 @@ function print_advice_report()
 	$destination = $_POST['PARAM_4'];
 	
 	$orientation = ($orientation ? 'L' : 'P');
-	$cols = array(2, 50, 180, 300, 440,515);
+	$cols = array(2, 50, 180, 320, 480, 515);
 	
 	
 	$headers=array(_('SlNo'), _('Employee Name'),  _('Job Position'), _('Department'), _('Payable'));
 	
-	$aligns = array('left', 'left', 'left',	'left', 'left');
+	$aligns = array('left', 'left', 'left',	'left', 'right');
 	
 	$rep = new FrontReport(_('Payment Advice Report'), "Payment advice Report", user_pagesize(), 9, $orientation);
 	
 	if ($orientation == 'L')
 		recalculate_cols($cols);
 			
-			$rep->Font();
-			$rep->Info($params,$cols,$headers,$aligns);
-			$rep->NewPage();
+	$rep->Font();
+	$rep->Info($params,$cols,$headers,$aligns);
+	$rep->NewPage();
+	
+	$sql = get_sql_for_payment_advices($employee,$jobposition,$department);
+	//display_notification($sql);exit;
+	$result = db_query($sql, "could not get payslips");
+
+	$slno = 1;
+	while($row = db_fetch($result)){
+
+		$rep->TextCol(0, 1,$slno);
+		$rep->TextCol(1, 2,$row['emp_name']);
+		$rep->TextCol(2, 3,$row['job_position']);
+		$rep->TextCol(3, 4,$row['department']);
+
+		$DisplayAmount = number_format2($row['amount'],$dec);
+
+		$rep->TextCol(4, 5, $DisplayAmount);
+
+		$rep->NewLine(1.2);
+		$slno++;
+	}
 			
-			
-			$rep->NewLine();
-			$rep->Line(5);
-			
-		$rep->End();
+	$rep->End();
 }
 ?>
