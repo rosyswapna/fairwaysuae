f745af1b85f32620ba0f70cf7496b4120ff0bd79
binu <binubabu@acube.com>
2015-06-24 14:52:01 +0530

    replist

--- modules/payroll/reporting/replist.php
+++ modules/payroll/reporting/replist.php
@@ -13,26 +13,26 @@ add_access_extensions();
 
 print_replist_report();
 
-function get_emp_list($emp, $department, $position)
+function get_empl_list($emp, $department, $position)
 {
 
 	$sql = "SELECT * FROM ".TB_PREF."employees as emp
 	
 	LEFT JOIN ".TB_PREF."passport as pt ON
-	pt.emp_id=emp.emp_id
+	pt.emp_id=emp.emp_id";
 	
 	/*LEFT JOIN ".TB_PREF."jobnames as jb ON
 	jb.job_name_id=emp.job_position_id
 	
 	LEFT JOIN ".TB_PREF."departments as dpt ON
-	dpt.dept_id=emp.department_id*/";
+	dpt.dept_id=emp.department_id*/
 	
 	if($department){
 		$sql .= " WHERE department_id=".db_escape($department);
 	}elseif($position){
-		$sql .= " WHERE job_position_id=".db_escape($department);
+		$sql .= " WHERE job_position_id=".db_escape($position);
 	}elseif($emp){
-		$sql .= " WHERE emp_id=".db_escape($emp);
+		$sql .= " WHERE emp.emp_id=".db_escape($emp);
 	}
 
 	return db_query($sql, "could not get employees");
@@ -56,7 +56,7 @@ function print_replist_report()
 	
 	$aligns = array('center', 'left', 'center',	'center','center');
 	
-	$rep = new FrontReport(_('Payment Advice Report'), "Payment advice Report", user_pagesize(), 9, $orientation);
+	$rep = new FrontReport(_('Detail Listing Report'), "Detail Listing Report", user_pagesize(), 9, $orientation);
 	
 	if ($orientation == 'L')
 		recalculate_cols($cols);
@@ -66,17 +66,19 @@ function print_replist_report()
 			$rep->NewPage();
 			
 			
-		$result = get_emp_list($employee,$department,$jobposition);
+		$result = get_empl_list($employee,$department,$jobposition);
+		
 	while ($myrow=db_fetch($result))
 	{
-
-		//$rep->Font('B');
-		$rep->TextCol(0, 1,_("").$myrow['emp_code']);
-		$rep->TextCol(1, 2,_("").$myrow['emp_first_name']." ".$myrow['emp_last_name']);
-		$rep->TextCol(2, 3,_("").$myrow['job_position_id']);
-		$rep->TextCol(3, 4,_("").$myrow['department_id']);
-		$rep->TextCol(4, 5,_("").$myrow['passport_no']);
-		//$rep->Font();
+		
+		
+			$rep->TextCol(0, 1,$myrow['emp_code']);
+			$rep->TextCol(1, 2,$myrow['emp_first_name']." ".$myrow['emp_last_name']);
+			$rep->TextCol(2, 3,$myrow['job_position_id']);
+			$rep->TextCol(3, 4,$myrow['department_id']);
+			$rep->TextCol(4, 5,$myrow['passport_no']);
+		
+		
 		$rep->NewLine(2);
 	}
 
